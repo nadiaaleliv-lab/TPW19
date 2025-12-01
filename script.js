41fbd0a3463813e666104dbcbe3bb03b3a0ef272
nadiaaleliv-lab <nadiaaleliv@gmail.com>
2025-12-01 13:20:36 -0500

    v.2

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         links: {
             github: 'https://github.com/nadiaaleliv-lab',
             linkedin: 'https://linkedin.com/in/usuariwww.linkedin.com/in/nadia-aleli-vega-reyes-a9320335b',
-            portfolio: 'portafolio',
+            portfolio: 'http://127.0.0.1:5500/TPW17-master/portafolio/index.html',
             email: 'nadiaaleliv@utp.edu.pe',
             cv: 'documents/cv.pdf',
             instagram: 'https://www.instagram.com/vegareyesnadia/'
